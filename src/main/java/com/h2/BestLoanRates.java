550df2f5dd7eb23c70249c61f812b768ff536736
Dheenat <dheenas@outlook.com>
2020-12-24 17:16:36 -0500

    Update BestLoanRates.java
    Completed module

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -8,7 +8,7 @@ public static Map<Integer, Float> bestRates  = Map.of(1, 5.50f, 2, 3.45f, 3, 2.6
 
     public static void main(String... args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name");
+        System.out.println("Enter your name");
         String name = scanner.nextLine();
         System.out.println("Hello" + name );
 
