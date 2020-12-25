b1e0705f0184b9a781a87d47f3352d59ca1c22ab
Dheenat <dheenas@outlook.com>
2020-12-24 21:51:35 -0500

    Update Finance.java
    added back

--- src/main/java/com/h2/Finance.java
+++ src/main/java/com/h2/Finance.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 public class Finance
 {
-
     public final static String BEST_LOAN_RATES = "bestLoanRates";
     public final static String SAVINGS_CALCULATOR  = "savingsCalculator";
     public final static String MORTGAGE_CALCULATOR  = "mortgageCalculator";
@@ -17,9 +16,9 @@ public class Finance
 
     public static void main( String[] args )
     {
-         boolean validationStatus =    validateCommandArguments(args);
+        boolean validationStatus =    validateCommandArguments(args);
 
-         if (validationStatus )
+        if (validationStatus )
             executeCommand(commandsToUsage.get(args[0]), Arrays.copyOfRange(args,1, args.length));
 
     }
@@ -28,8 +27,7 @@ public class Finance
         String appName = args[0];
         switch (appName){
             case BEST_LOAN_RATES:
-            return args.length == 1 ? true:false;
-               // break;
+                return args.length == 1 ? true:false;
             case SAVINGS_CALCULATOR:
                 return args.length == 3 ? true:false;
             case MORTGAGE_CALCULATOR:
@@ -39,21 +37,20 @@ public class Finance
         }
     }
     private static  void executeCommand(String command  , String[] arguments ){
-    switch (command){
-        case BEST_LOAN_RATES :
-            System.out.println("Finding best loan rates ...");
-            BestLoanRates.main(arguments);
-            return;
-        case SAVINGS_CALCULATOR:
-
-            System.out.println("Finding your net savings ...");
-            SavingsCalculator.main(arguments);
-            return;
-         case MORTGAGE_CALCULATOR:
-             System.out.println("Finding your monthly payment ...");
-             MortgageCalculator.main(arguments);
-             return;
-    }
+        switch (command){
+            case BEST_LOAN_RATES :
+                System.out.println("Finding best loan rates ...");
+                BestLoanRates.main(arguments);
+                return;
+            case SAVINGS_CALCULATOR:
+                System.out.println("Finding your net savings ...");
+                SavingsCalculator.main(arguments);
+                return;
+            case MORTGAGE_CALCULATOR:
+                System.out.println("Finding your monthly payment ...");
+                MortgageCalculator.main(arguments);
+                return;
+        }
 
     }
 }
