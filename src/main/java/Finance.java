ff7387ec146bec891d4540d59e129d601a263957
Dheenat <dheenas@outlook.com>
2020-12-24 22:21:25 -0500

    Completed module

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -5,52 +5,32 @@ import com.h2.SavingsCalculator;
 import java.util.Arrays;
 import java.util.Map;
 
-public class Finance
-{
-    public final static String BEST_LOAN_RATES = "bestLoanRates";
-    public final static String SAVINGS_CALCULATOR  = "savingsCalculator";
-    public final static String MORTGAGE_CALCULATOR  = "mortgageCalculator";
+public class Finance {
+    public static final String BEST_LOAN_RATES = "bestLoanRates";
+    public static final String SAVINGS_CALCULATOR = "savingsCalculator";
+    public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
-    public final static Map<String, String> commandsToUsage  = Map.of(
+    public static final Map<String, String> commandsToUsage = Map.of(
             BEST_LOAN_RATES, "usage: bestLoanRates",
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
-            MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
+            MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
+    );
 
-    public static void main( String[] args )
-    {
-        String command = args[0];
-
-        if( !  commandsToUsage.containsKey(command) ) {
-            System.out.println(command + ": command not found");
-            return ;
-        }
-        boolean isValidationCommand=    validateCommandArguments(args);
-
-        if(!isValidationCommand) {
-            System.out.println(commandsToUsage.get(args[0]));
-            return;
-        }
-
-            executeCommand(commandsToUsage.get(command), Arrays.copyOfRange(args,1, args.length));
-
-    }
-
-    private static boolean validateCommandArguments(String[] args){
-        String appName = args[0];
-        switch (appName){
+    private static boolean validateCommandArguments(String[] args) {
+        switch (args[0]) {
             case BEST_LOAN_RATES:
-                return args.length == 1 ;
+                return args.length == 1;
             case SAVINGS_CALCULATOR:
-                return args.length == 3 ;
+                return args.length == 3;
             case MORTGAGE_CALCULATOR:
-                return args.length == 4 ;
-            default:
-                return  false;
+                return args.length == 4;
         }
+        return false;
     }
-    private static  void executeCommand(String command  , String[] arguments ){
-        switch (command){
-            case BEST_LOAN_RATES :
+
+    private static void executeCommand(String command, String[] arguments) {
+        switch (command) {
+            case BEST_LOAN_RATES:
                 System.out.println("Finding best loan rates ...");
                 BestLoanRates.main(arguments);
                 return;
@@ -63,6 +43,21 @@ public class Finance
                 MortgageCalculator.main(arguments);
                 return;
         }
+    }
 
+    public static void main(String[] args) {
+        String command = args[0];
+        if(!commandsToUsage.containsKey(command)) {
+            System.out.println(command + ": command not found");
+            return;
+        }
+
+        boolean isValidCommand = validateCommandArguments(args);
+        if(!isValidCommand) {
+            System.out.println(commandsToUsage.get(args[0]));
+            return;
+        }
+
+        executeCommand(command, Arrays.copyOfRange(args, 1, args.length));
     }
 }
